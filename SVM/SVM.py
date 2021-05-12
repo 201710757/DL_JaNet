78edc5a4ee0397a088e9602c068461343523bc88
201710757 <neti69@naver.com>
2021-05-13 02:23:51 +0900

    SVM - embedding source

--- SVM/SVM.py
+++ SVM/SVM.py
@@ -2,16 +2,32 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 import sklearn as sk
 from sklearn.svm import SVC
+from sklearn import metrics
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Normalizer
 
-embs = np.load('.npy')
-labels = np.load('.npy')
 
+embs = np.load('./emb.npy', allow_pickle=True)
+labels = np.load('./label.npy', allow_pickle=True)
+print(embs.shape)
+print(labels.shape)
 # test_emb = np.load('.npy')
 # test_label = np.load('.npy')
 
 X_train, X_test, Y_train, Y_test = train_test_split(embs, labels, test_size=0.33, random_state=321)
-
+print("ss : ",X_train.shape, Y_train.shape)
+print("test : ",X_test.shape, Y_test.shape)
 svc_1 = SVC(kernel='linear')
+
+
+in_encoder = Normalizer(norm='l2')
+X_train = in_encoder.transform(X_train)
+X_test = in_encoder.transform(X_test)
+
+out_encoder = LabelEncoder()
+out_encoder.fit(Y_train)
+Y_train = out_encoder.transform(Y_train)
+Y_test = out_encoder.transform(Y_test)
 
 def train_and_evaluate(clf,X_train,X_test,Y_train,Y_test):
     
@@ -36,5 +52,5 @@ joblib.dump(svc_1, 'svc_face.pkl')
 # x = embData
 # #y = labelData
 svc = joblib.load('svc_face.pkl')
-svc.predict(x)
+# svc.predict(x)
 
--- mtcnn_facenet/get_emb_image.py
+++ mtcnn_facenet/get_emb_image.py
@@ -1,7 +1,6 @@
 from facenet_pytorch import MTCNN, InceptionResnetV1
 import torch
 import numpy as np
-import mmcv, cv2
 from PIL import Image, ImageDraw
 from IPython import display
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -10,66 +9,52 @@ print('Running on device: {}'.format(device))
 mtcnn = MTCNN(device=device)
 model = InceptionResnetV1(pretrained='vggface2').eval()
 
-img = Image.open('sample.jpg')
-
-img_cropped = mtcnn(img)
+# img = Image.open('sample.jpg')
+# img_cropped = mtcnn(img)
 
 model.classify = True
-img_probs = model(img_cropped.unsqueeze(0))
-print(img_probs)
+# img_probs = model(img_cropped.unsqueeze(0))
+# print(img_probs)
 
-emb = []
 def get_embedding(filename):
-    img = Image.open(file_name)
+    img = Image.open(filename)
     img_cropped = mtcnn(img)
 
     model.classify = True
     img_probs = model(img_cropped.unsqueeze(0))
-    emb.append(img_probs)
+    return img_probs[0].detach().numpy()
 
 
 
 import os
+embs = []
+labels = []
 
-path = "./train"
+path = "../data/train"
 folder_list = os.listdir(path)
  
 for i in folder_list:
+    if i == ".DS_Store":
+        continue
     _path = path + "/" + i
     file_list = os.listdir(_path)
     
     for _file in file_list:
-        get_embedding(_path+"/"+_file)
+        if _file == ".DS_Store":
+            continue
+        print("Embedding : {}".format(_path+"/"+_file))
+        embs.append(get_embedding(_path+"/"+_file))
+        labels.append(i)
 
+e = np.array(embs)
+print(e[0])
+np.save('emb.npy', e)
+l = np.array(labels)
+print(l[0])
+np.save('label.npy', l)
 
 
 
 
 import sys
 sys.exit(0)
-
-capture = cv2.VideoCapture(0)
-#capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-#capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-
-while cv2.waitKey(33) < 0:
-    ret, frame = capture.read()
-    cap = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    frame = Image.fromarray(cap)#cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-     
-    boxes, _ = mtcnn.detect(frame)
-    frame_draw = frame.copy()
-    draw = ImageDraw.Draw(frame_draw)
-    try:
-        for box in boxes:
-            draw.rectangle(box.tolist(), outline=(255, 0, 0), width=6)
-    except Exception as e:
-        pass
-    
-    n_img = np.array(frame_draw)
-
-    cv2.imshow("VideoFrame", cv2.cvtColor(np.array(frame_draw), cv2.COLOR_RGB2BGR))
-
-capture.release()
-cv2.destroyAllWindows()
